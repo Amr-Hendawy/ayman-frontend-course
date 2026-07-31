245986a051a44399e986248a94a2b1f2f61ba58f
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-07-31 20:45:57 +0300

    complete arrray

--- array.js
+++ array.js
@@ -1,1 +1,77 @@
-let x = 10;
+// let pals = [
+//   "Amr",
+//   "Samy",
+//   "Waleed",
+//   1,
+//   5,
+//   true,
+//   function (params) {
+//     console.log("Hello");
+//   },
+//   { name: "Amr" },
+//   ["Ameer", "Haitham", "Osama"],
+// ];
+
+// console.log(pals[3][0][0][1]);
+// console.log(`Hello ${pals[3][0][0][1]}`);
+
+// pals[3] = "Waleed";
+
+// console.log(pals);
+
+// pals[3] = ["Waleed", "Ayman"];
+// console.log(pals);
+
+// console.log(Array.isArray(pals));
+
+// if (!Array.isArray(pals) === true) {
+//   console.log("Fetch Data Successfully");
+// } else {
+//   console.log("Something Went Wrong");
+// }
+// pals.length = 2;
+
+// console.log(pals);
+
+// console.log(pals);
+// pals.unshift("Ali");
+// console.log(pals);
+// pals.shift();
+// console.log(pals);
+// pals.push("A", "B", "C");
+// console.log(pals);
+// pals.pop();
+// console.log(pals);
+
+// console.log(pals);
+// console.log(pals.indexOf("Ameer", 2));
+
+// console.log(pals.lastIndexOf("Ameer", -1));
+
+// if (pals.lastIndexOf("Osamas") === -1) {
+//   console.log("Member Not Found");
+// }
+
+// console.log(pals);
+// console.log(pals.sort().reverse());
+
+// let pals = ["Zyad", "Ali", "Ameer", "Haitham", "Osama"];
+// let newPals = ["Gamal", "Tamer"];
+// let schoolPals = ["Mahmoud", "Zayn"];
+
+// console.log(pals);
+// console.log(pals.slice());
+// console.log(pals.slice(1, 3));
+// console.log(pals.slice(-3, -1));
+
+// pals.splice(1, 2, "Sameer", "Waheed");
+// console.log(pals);
+
+// let allPals = pals.concat(newPals, schoolPals, "Seleem", [1, 2, 3]);
+
+// console.log(allPals);
+// console.log(allPals.join());
+// console.log(allPals.join(""));
+// console.log(allPals.join(" - "));
+// console.log(allPals.join(" / "));
+// console.log(allPals.join(" $ ").toUpperCase());

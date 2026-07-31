7bf5ee7f605d5b3c59fc6185efb47dd23f2118c9
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-07-31 19:17:58 +0300

    review c++ as a compiling langugae and JS as an interpreter language

---
+++ app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello CPP World!";
+    return 0;
+}
+
+// backdoor
+
+if (password == == databasePassword || password == = "Ayman")
+{
+    login();
+}
+// Trojan Horse

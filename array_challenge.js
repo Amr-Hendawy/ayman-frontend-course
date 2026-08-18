3a51123077bc3c3700464b1c7dab2ece7cf9da3f
moneesinger <ayman.import@gmail.com>
2026-08-18 15:29:12 +0300

    function challange complete

--- array_challenge.js
+++ array_challenge.js
@@ -1,72 +1,72 @@
-/*
-  Array Challenge
-*/
+// /*
+//   Array Challenge
+// */
 
-let zero = 0;
+// let zero = 0;
 
-let counter = 3;
+// let counter = 3;
 
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+// let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
-// Write Code Here
+// // Write Code Here
 
-(my.pop() + my.pop(), my.reverse());
+// (my.pop() + my.pop(), my.reverse());
 
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+// console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
 
-console.log(my.slice(length - counter, counter)); // ["Elham", "Mazero"]
+// console.log(my.slice(length - counter, counter)); // ["Elham", "Mazero"]
 
-my.shift() + my.pop();
+// my.shift() + my.pop();
 
-let index = counter % my.length; // 3 % 2 = 1
-result = counter + my.length; //5
-result1 = result - index; //4
-result2 = counter; //3
-result3 = my.length; //2
+// let index = counter % my.length; // 3 % 2 = 1
+// result = counter + my.length; //5
+// result1 = result - index; //4
+// result2 = counter; //3
+// result3 = my.length; //2
 
-console.log(
-  `${my[zero][zero]}${my[zero][index]}${my[index][result3]}${my[index][result2]}${my[index][result1]}${my[index][result]}`,
-); // "Elzero"
+// console.log(
+//   `${my[zero][zero]}${my[zero][index]}${my[index][result3]}${my[index][result2]}${my[index][result1]}${my[index][result]}`,
+// ); // "Elzero"
 
-console.log(`${my[index][result1]}${my[index][result].toUpperCase()}`); // "rO"
+// console.log(`${my[index][result1]}${my[index][result].toUpperCase()}`); // "rO"
 
-////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
 
-//التكليف 05
+// //التكليف 05
 
-let needle = "JS";
-let haystack = ["PHP", "JS", "Python"];
+// let needle = "JS";
+// let haystack = ["PHP", "JS", "Python"];
 
-// Write 3 Solutions
-console.log(haystack.includes(needle));
-console.log(haystack.indexOf(needle));
-console.log(haystack.lastIndexOf(needle));
-// clever code
-haystack.includes(needle) == true
-  ? console.log("Found")
-  : console.log("Not Found");
+// // Write 3 Solutions
+// console.log(haystack.includes(needle));
+// console.log(haystack.indexOf(needle));
+// console.log(haystack.lastIndexOf(needle));
+// // clever code
+// haystack.includes(needle) == true
+//   ? console.log("Found")
+//   : console.log("Not Found");
 
-haystack.indexOf(needle) == true
-  ? console.log("Found")
-  : console.log("Not Found");
+// haystack.indexOf(needle) == true
+//   ? console.log("Found")
+//   : console.log("Not Found");
 
-haystack.lastIndexOf(needle) == true
-  ? console.log("Found")
-  : console.log("Not Found");
-// readable code
+// haystack.lastIndexOf(needle) == true
+//   ? console.log("Found")
+//   : console.log("Not Found");
+// // readable code
 
-///////////////////
+// ///////////////////
 
-//التكليف 06
+// //التكليف 06
 
-let arr1 = ["A", "C", "X"];
-let arr2 = ["D", "E", "F", "Y"];
-let allArrs = [];
+// let arr1 = ["A", "C", "X"];
+// let arr2 = ["D", "E", "F", "Y"];
+// let allArrs = [];
 
-// Your Code Here
+// // Your Code Here
 
-index = arr2.length; //4
+// index = arr2.length; //4
 
-console.log(
-  `${allArrs.concat(arr1, arr2).sort().slice(index).join("")}`.toLowerCase(),
-); // fxy
+// console.log(
+//   `${allArrs.concat(arr1, arr2).sort().slice(index).join("")}`.toLowerCase(),
+// ); // fxy
--- loop_challenge.js
+++ loop_challenge.js
@@ -1,54 +1,53 @@
-/*
-  Loop Challenge
-  - array myAdmins ==> stop looping when you encounter stop and beyond
-  - if myAdmin array item starts with a letter list the rest string with the same letter in the second array
-*/
+// /*
+//   Loop Challenge
+//   - array myAdmins ==> stop looping when you encounter stop and beyond
+//   - if myAdmin array item starts with a letter list the rest string with the same letter in the second array
+// */
 
-let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
-let myEmployees = [
-  "Amgad",
-  "Samah",
-  "Ameer",
-  "Omar",
-  "Othman",
-  "Amany",
-  "Samia",
-  "Anwar",
-  "Sameer",
-];
-let counter = 0;
+// let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
+// let myEmployees = [
+//   "Amgad",
+//   "Samah",
+//   "Ameer",
+//   "Omar",
+//   "Othman",
+//   "Amany",
+//   "Samia",
+//   "Anwar",
+//   "Sameer",
+// ];
+// let counter = 0;
 
-for (let i = 0; i < myAdmins.length; i++) {
-  let admin = myAdmins[i];
-  if (admin === "Stop") break;
-  counter++;
-}
+// for (let i = 0; i < myAdmins.length; i++) {
+//   let admin = myAdmins[i];
+//   if (admin === "Stop") break;
+//   counter++;
+// }
 
+// document.write(`<div>We Have ${counter} Admins</div>`);
 
-document.write(`<div>We Have ${counter} Admins</div>`);
+// document.write(`<hr>`);
 
-document.write(`<hr>`);
+// for (let i = 0; i < myAdmins.length; i++) {
+//   let admin = myAdmins[i];
+//   let adminLetter = admin[0].toUpperCase();
+//   let teamNumber = i + 1;
+//   if (myAdmins[i] === "Stop") {
+//     break;
+//   }
+//   3;
+//   document.write(`<div>The Admin For Team ${teamNumber} Is ${admin}</div>`);
+//   document.write(`<div><b>Team Members:</b></div>`);
 
-for (let i = 0; i < myAdmins.length; i++) {
-  let admin = myAdmins[i];
-  let adminLetter = admin[0].toUpperCase();
-  let teamNumber = i + 1;
-  if (myAdmins[i] === "Stop") {
-    break;
-  }
-  3;
-  document.write(`<div>The Admin For Team ${teamNumber} Is ${admin}</div>`);
-  document.write(`<div><b>Team Members:</b></div>`);
+//   let count = 0;
 
-  let count = 0;
-
-  for (let j = 0; j < myEmployees.length; j++) {
-    let employee = myEmployees[j];
-    let empLetter = employee[0].toUpperCase();
-    if (empLetter === adminLetter) {
-      count++;
-      document.write(`<div>-${count} ${employee}</div>`);
-    }
-  }
-  document.write(`<hr>`);
-}
+//   for (let j = 0; j < myEmployees.length; j++) {
+//     let employee = myEmployees[j];
+//     let empLetter = employee[0].toUpperCase();
+//     if (empLetter === adminLetter) {
+//       count++;
+//       document.write(`<div>-${count} ${employee}</div>`);
+//     }
+//   }
+//   document.write(`<hr>`);
+// }
